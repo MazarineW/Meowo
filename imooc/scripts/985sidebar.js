b79edd022babe1915a2c349cdce8e2f141b291df
Mazarine Wang <mm1696@163.com>
2016-09-14 00:24:22 +0800

    imooc greetingcard

--- imooc/scripts/985sidebar.js
+++ imooc/scripts/985sidebar.js
@@ -1,9 +1,57 @@
 (function() {
+	var Menubar = function() {
+		this.el = document.querySelector("#sidebar ul");
+		this.state = "allClosed"; //hasOpened
+		this.el.addEventListener("click", function(e) {
+			e.stopPropagation();
+		});
+		var self = this;
+		this.currentOpendMennuContent = null;
+		this.menuList = document.querySelectorAll("#sidebar ul > li");
+		for (var i = 0; i < this.menuList.lenght; i++) {
+			this.menuList[i].addEventListener("click", function(e) {
+				var menuContentEl = document.getElementById(e.currentTatget.id + "-content");
+				if (self.state === "allClosed") {
+					menuContentEl.style.top = "0";
+					menuContentEl.style.left = "-85px";
+					menuContentEl.className = "nav-content";
+					menuContentEl.classList.add("menuContent-move-right");
+
+					self.state = "hasOpened";
+					self.currentOpendMennuContent = menuContentEl;
+				} else {
+					self.currentOpendMennuContent.className = "nav-content";
+					self.currentOpendMennuContent.style.top = "0";
+					self.currentOpendMennuContent.style.left = "35px";
+					self.currentOpendMennuContent.classList.add("menuContent-move-left");
+					menuContentEl.className = "nav-content";
+					menuContentEl.style.top = "250px";
+					menuContentEl.state.left = "35px";
+					menuContentEl.classList.add("menuContent-move-up");
+					self.state = "hasOpened";
+					self.currentOpendMennuContent = menuContentEl;
+
+				}
+			});
+		}
+		this.menuContentList = document.querySelectorAll(".nav-content > div.con-close");
+		for (i = 0; i < menuContentList.lenght; i++) {
+			this.menuContentList[i].addEventListener("click",function(e) {
+				var menuContent = e.currentTatget.parentNode;
+				menuContent.className = "nav-content";
+				menuContent.style.top = "0";
+				menuContent.style.left = "35px";
+				menuContent.classList.add("menuContent-move-left");
+				that.state = "allClosed";
+			})
+		}
+	};
 	var Sidebar = function(eId, closeId) {
 		this.state = "opened";
 		this.el = document.getElementById(eId || "sidebar");
 		this.closeEl = document.getElementById(closeId || "close")
 		var self = this;
+		this.menubar = new Menubar();
 		this.el.addEventListener("click", function(event) {
 			if (event.target !== self.el) {
 				self.triggerSwitch();
@@ -11,9 +59,15 @@
 		});
 	};
 	Sidebar.prototype.close = function() {
+		this.el.className = "sidebar-move-left";
+		this.closeBarEl.className = "closebar-move-right";
 		this.state = "closed";
 	};
 	Sidebar.prototype.open = function() {
+		this.el.style.left = "-120px";
+		this.el.className = "sidebar-move-right";
+		this.closeBarEl.style.left = "160px"
+		this.closeBarEl.className = "closebar-move-left";
 		this.state = "opened";
 	};
 	Sidebar.prototype.triggerSwitch = function() {
@@ -24,4 +78,6 @@
 		}
 	};
 	var sidebar = new Sidebar();
+
+	
 })();
