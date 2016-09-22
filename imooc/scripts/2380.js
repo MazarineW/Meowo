9a9e6e7ce05fee3c327490bab8643e56ab769f6d
Mazarine Wang <mm1696@163.com>
2016-09-22 23:05:14 +0800

    imooc

---
+++ imooc/scripts/2380.js
@@ -0,0 +1,12 @@
+function getPosition(node) {
+	var left = node.offsetLeft;
+	var top = node.offsetTop;
+	var parent = node.offsetParent;
+	while (parent != null) {
+		left += parent.offsetLeft;
+		top += parent.offsetTop;
+		parent = parent.offsetParent;
+	}
+	return {"left": left, "top": top};
+}
+    
